4f3bcb08b9e633ed27ee0ebc6df6b2b6aebc2b9a
samirabarrera <samira.barrera@galileo.edu>
2024-12-16 23:28:19 -0600

    adding js code

---
+++ servidor.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',   
+    user: 'userapp',  
+    password: 'Ccna2024*',     
+    database: 'BancoDB_Tarea'  // Nombre exacto de la base de datos en MySQL
+});
+
+const closeConnection = () => {
+    connection.end((err) => {
+        if (err) {
+            console.error("Error al conectar a la base de datos: ", err);
+        } else {
+            console.log("Conexión cerrada");
+        }
+    });
+}
+
+connection.query('SELECT * FROM clientes', (err, results, fields) => {
+    if (err) {
+        console.error("Hubo un error en query: ", err);
+    } else {
+        console.log("resultados: ", results);
+        console.log("Campos: ", fields);
+    }
+    closeConnection
+});
